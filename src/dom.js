c39ebf98e488ffe4ddfd3adec6276c5098d45b0f
mosmn <ithinkisee8@gmail.com>
2023-06-18 00:33:54 +0800

    added some styling

--- src/dom.js
+++ src/dom.js
@@ -7,15 +7,6 @@ const CreateNewElement = (tag, className) => {
   return element;
 };
 
-const search = () => {
-  const search = CreateNewElement("div", "search");
-  search.innerHTML = `
-    <input type="text" class="search-input" placeholder="Search for location">
-    <button class="search-btn">Search</button>
-    `;
-  return search;
-};
-
 const weatherSearch = () => {
     const searchBtn = document.querySelector(".search-btn");
     const searchInput = document.querySelector(".search-input");
@@ -44,63 +35,58 @@ const weatherSearch = () => {
   };
   
   const updateWeatherInfo = (weather) => {
+    const container = document.querySelector(".container");
     const weatherInfoContainer = document.querySelector(".weather-info-container");
     if (weatherInfoContainer) {
-      weatherInfoContainer.remove();
-    }
-  
+        weatherInfoContainer.remove();
+        }
     const weatherInfoContainerNew = CreateNewElement("div", "weather-info-container");
     weatherInfoContainerNew.appendChild(weatherInfo(weather));
-    document.body.appendChild(weatherInfoContainerNew);
+    container.appendChild(weatherInfoContainerNew);
   };
   
   const weatherInfo = (weather) => {
     const weatherInfo = CreateNewElement("div", "weather-info");
     weatherInfo.innerHTML = `
       <div class="weather-info__location">
-          <h1 class="weather-info__location-name">${weather.location.name}</h1>
-          <h2 class="weather-info__location-region">${weather.location.region}</h2>
-          <h3 class="weather-info__location-country">${weather.location.country}</h3>
-      </div>
-      <div class="weather-info__condition">
-          <img src="${weather.current.condition.icon}" alt="${weather.current.condition.text}" class="weather-info__condition-img">
-          <h3 class="weather-info__condition-text">${weather.current.condition.text}</h3>
+          <h1 class="weather-info__location-region">${weather.location.region}</h1>
       </div>
       <div class="weather-info__temp">
           <h1 class="weather-info__temp-value">${weather.current.temp_c}</h1>
           <h3 class="weather-info__temp-unit">°C</h3>
       </div>
+      <div class="weather-info__condition">
+      <h3 class="weather-info__condition-text">${weather.current.condition.text}</h3>
+      <img src="${weather.current.condition.icon}" alt="${weather.current.condition.text}" class="weather-info__condition-img">
+  </div>
       `;
     return weatherInfo;
   }
   
 
 const footer = () => {
-  const footer = CreateNewElement("footer", "footer");
-  const footerText = CreateNewElement("p", "footer-text");
-  footerText.textContent = "Powered by ";
-  const footerLink = CreateNewElement("div", "footer-link");
-  footerLink.innerHTML =
-    '<a href="https://www.weatherapi.com/" title="Free Weather API"><img src="//cdn.weatherapi.com/v4/images/weatherapi_logo.png" alt="Weather data by WeatherAPI.com" border="0"></a>';
-  footer.appendChild(footerText);
-  footer.appendChild(footerLink);
+  const footer = document.querySelector(".footer");
+    footer.innerHTML = `
+    <h3 class="footer__text">Powered by</h3>
+    <a href="https://www.weatherapi.com/" title="Free Weather API"><img src="//cdn.weatherapi.com/v4/images/weatherapi_logo.png" alt="Weather data by WeatherAPI.com" border="0"></a>
+    `;
   return footer;
 };
 
 const loadPage = () => {
     const container = CreateNewElement("div", "container");
     container.innerHTML = `
-        <div class="weather-container">
-            <div class="weather-container__search"></div>
-            <div class="weather-container__info"></div>
+        <div class="search-container">
+            <input type="text" class="search-input" placeholder="Search for location">
+            <img src="https://img.icons8.com/ios-filled/50/000000/search--v1.png" class="search-btn">
         </div>
+        <div class="weather-info-container"></div>
+        <div class="footer"></div>
         `;
     document.body.appendChild(container);
-    const searchContainer = document.querySelector(".weather-container__search");
-    searchContainer.appendChild(search());
-    weatherSearch();
     defaultWeather();
-    document.body.appendChild(footer());
+    weatherSearch();
+    footer();
 };
 
 document.addEventListener("DOMContentLoaded", loadPage);
